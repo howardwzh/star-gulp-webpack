d097fcf48601907b5d0d8f66325091dd1b83ff6f
王忠华 <wangzhonghua@ppmoney.com>
2018-05-28 11:33:28 +0800

    update

--- demos/loaders/my-css-loader/index.js
+++ demos/loaders/my-css-loader/index.js
@@ -1,12 +1,23 @@
-var loaderUtils = require('loader-utils')
-var fs = require('fs')
+const getOptions = require('loader-utils').getOptions
+const validateOptions = require('schema-utils')
+const fs = require('fs')
+
+const schema = {
+  type: 'object',
+  properties: {
+    pxtorem: {
+      rootValue: 'number'
+    }
+  }
+}
 
 module.exports = function (source) {
-  const options = loaderUtils.getOptions(this)
-  console.log(options)
-  console.log(source)
+  const options = getOptions(this)
+  // console.log(options)
+  // console.log(source)
+  validateOptions(schema, options, 'Example Loader')
   source = source.replace(/(\d+)px/g, (value) => {
-    return `${value.split('px')[0]/50}rem` 
+    return `${value.split('px')[0] / options.pxtorem.rootValue}rem` 
   })
   return source
 }
--- demos/loaders/my-html-loader/index.js
+++ demos/loaders/my-html-loader/index.js
@@ -3,13 +3,13 @@ var fs = require('fs')
 
 module.exports = function (source) {
   const options = loaderUtils.getOptions(this)
-  console.log(options)
-  console.log(source)
+  // console.log(options)
+  // console.log(source)
   let layoutHtml = ''
   options.layouts.forEach(l => {
     layoutHtml += fs.readFileSync(l, 'utf-8')
   });
   source = source.replace('{{ __title__ }}', options.title).replace('{{ __content__ }}', layoutHtml)
-  console.log(source)
+  // console.log(source)
   return source
 }
