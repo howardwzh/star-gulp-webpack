b0dc82ae83c9ced7a18d373c094b0ae3cf077005
王忠华 <wangzhonghua@ppmoney.com>
2018-05-28 12:16:34 +0800

    feat: my-js-loader逻辑编写

--- demos/loaders/my-js-loader/index.js
+++ demos/loaders/my-js-loader/index.js
@@ -1,3 +1,5 @@
 module.exports = function (source) {
+  source = source.replace(/\/\/.*/, '').replace(/\/\*[\s\S]*\*\//, '')
+  console.log(source)
   return source;
 }
--- demos/src/index.js
+++ demos/src/index.js
@@ -1,4 +1,16 @@
 import './index.html'
 import './index.css'
 
-const test = ['aaaa', 'bbbb']
+const test = ['aaaa', 'bbbb'] // 注释1
+
+/** 注释2 */
+const test2 = ['aaaa2', 'bbbb2']
+
+/**
+ * test3
+ * 
+ * @param {any} name 
+ */
+function test3 (name) {
+  console.log(name)
+}
